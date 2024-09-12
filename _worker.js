07017fd9455091df6c0209187d0d81f754cda9fe
DĀÑTÊ X-MÚGÊTSÙ <48172506+dantemugetsu@users.noreply.github.com>
2024-09-13 03:25:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'b7122b11-f5b4-4948-90cc-499177104091';
 
-const พร็อกซีไอพีs = ['155.248.182.42'];
+const พร็อกซีไอพีs = ['104.248.145.216'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
